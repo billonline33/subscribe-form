436cc905e9724c4969343bdc76ece10d8c6b7124
billonline33 <billonline33@hotmail.com>
2019-12-07 16:54:16 +1100

    framework: setup eslint and prettier
    install eslint and prettier vscode ententions
    create .eslintrc and .prettierrc
    npm install --save-dev prettier eslint-config-prettier eslint-plugin-prettier

---
+++ src/components/subscribe/SubscribeForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class SubscribeFrom extends Component {
+
+
+    render() {
+        return  (<div>
+            <p> The form works</p>
+        </div>)
+
+    }
+}
+
+export default SubscribeFrom
